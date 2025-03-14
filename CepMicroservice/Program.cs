560209d6e6357276d7fc6f1841035ab39e5de31c
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2025-03-14 16:36:56 -0300

    Configure DI

--- CepMicroservice/Program.cs
+++ CepMicroservice/Program.cs
@@ -1,24 +1,19 @@
+using CepMicroservice.Data;
+using CepMicroservice.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddScoped<CorreiosApiService>();
+builder.Services.AddScoped<AddressService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=cepdb.sqlite"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
