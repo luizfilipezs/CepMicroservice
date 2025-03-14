e3a6b719b36dea462ed1d20d47c4009cbc987129
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2025-03-14 16:37:30 -0300

    Create AppDbContext

---
+++ CepMicroservice/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CepMicroservice.Models;
+
+namespace CepMicroservice.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>().HasIndex(a => a.Cep).IsUnique();
+        }
+    }
+}
