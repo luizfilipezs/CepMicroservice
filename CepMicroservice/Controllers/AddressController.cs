64216df1e9de92cc5bda4d8b1202407101b4f3eb
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2025-03-14 16:37:47 -0300

    Create Address resource

---
+++ CepMicroservice/Controllers/AddressController.cs
@@ -0,0 +1,29 @@
+﻿using CepMicroservice.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace CepMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/address")]
+    public class AddressController(AddressService addressService, CorreiosApiService correiosApiService) : ControllerBase
+    {
+        private readonly AddressService _addressService = addressService;
+        private readonly CorreiosApiService _correiosApiService = correiosApiService;
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> GetAddress(string cep)
+        {
+            cep = Regex.Replace(cep, @"\D", ""); // keep only digits
+
+            var address = await _addressService.GetByCepAsync(cep);
+            if (address != null) return Ok(address);
+
+            address = await _correiosApiService.GetAddressFromCorreiosAsync(cep);
+            if (address == null) return NotFound();
+
+            await _addressService.SaveAsync(address);
+            return Ok(address);
+        }
+    }
+}
---
+++ CepMicroservice/Services/AddressService.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using CepMicroservice.Data;
+using CepMicroservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CepMicroservice.Services
+{
+    public class AddressService(AppDbContext context)
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<Address?> GetByCepAsync(string cep)
+        {
+            return await _context.Addresses.FirstOrDefaultAsync(a => a.Cep == cep);
+        }
+
+        public async Task SaveAsync(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
