59093cda83f28d78708ecf64f8c5e1bd43cacbaa
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2025-03-14 16:37:57 -0300

    Create CorreiosApiService

---
+++ CepMicroservice/Services/CorreiosApiService.cs
@@ -0,0 +1,26 @@
+﻿using CepMicroservice.Models;
+
+namespace CepMicroservice.Services
+{
+    public class CorreiosApiService(HttpClient httpClient)
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<Address?> GetAddressFromCorreiosAsync(string cep)
+        {
+            var url = $"https://viacep.com.br/ws/{cep}/json/";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadFromJsonAsync<Address>();
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+        }
+    }
+}
