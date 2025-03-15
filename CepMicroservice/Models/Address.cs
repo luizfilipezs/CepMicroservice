b1eda39a94daacf6cdb1f0984a8efd3fa2b0fe2d
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2025-03-14 21:45:23 -0300

    Map Address properties for JSON parsing

--- CepMicroservice/Models/Address.cs
+++ CepMicroservice/Models/Address.cs
@@ -1,12 +1,24 @@
-﻿namespace CepMicroservice.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CepMicroservice.Models
 {
     public class Address
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("cep")]
         public string Cep { get; set; } = string.Empty;
+
+        [JsonPropertyName("logradouro")]
         public string Logradouro { get; set; } = string.Empty;
+
+        [JsonPropertyName("bairro")]
         public string Bairro { get; set; } = string.Empty;
+
+        [JsonPropertyName("localidade")]
         public string Cidade { get; set; } = string.Empty;
+
+        [JsonPropertyName("estado")]
         public string Estado { get; set; } = string.Empty;
     }
 }
